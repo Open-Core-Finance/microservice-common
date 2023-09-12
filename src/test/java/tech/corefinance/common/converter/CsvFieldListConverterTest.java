68382088c042746fc739d8c63933d8ae6f37c82f
Trung Doan <trung_bao.d@kbtgvn.tech>
2023-09-12 16:25:06 +0700

    Add mỏe unit tests

---
+++ src/test/java/tech/corefinance/common/converter/CsvFieldListConverterTest.java
@@ -0,0 +1,39 @@
+package tech.corefinance.common.converter;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import tech.corefinance.common.dto.CsvExportDefinition;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.Locale;
+
+public class CsvFieldListConverterTest {
+
+    private CsvFieldListConverter csvFieldListConverter;
+
+    @BeforeEach
+    public void setUp() {
+        csvFieldListConverter = new CsvFieldListConverter();
+    }
+
+    @Test
+    public void test_parse_normal() {
+        var input = "[{\"title\": \"Product name\", \"fieldName\": \"name\"}]";
+        var outputArr = csvFieldListConverter.parse(input, null);
+        assertTrue(outputArr.length == 1);
+        var output = outputArr[0];
+        assertEquals("name", output.getFieldName());
+    }
+
+    @Test
+    public void test_parse_null() {
+        assertThrows(IllegalArgumentException.class, () -> csvFieldListConverter.parse(null, null));
+    }
+
+    @Test
+    public void test_print() {
+        var input = new CsvExportDefinition("Description", "description");
+        var output = csvFieldListConverter.print(new CsvExportDefinition[]{input}, null);
+        assertEquals("[{\"title\":\"Description\",\"fieldName\":\"description\"}]", output);
+    }
+}
