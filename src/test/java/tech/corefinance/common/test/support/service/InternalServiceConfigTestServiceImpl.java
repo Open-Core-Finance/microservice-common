f0f01e83b3584bfbae491d629124499d6c48a9cc
Trung Doan <trung_bao.d@kbtgvn.tech>
2023-10-03 15:30:51 +0700

    Clean unused import

--- src/test/java/tech/corefinance/common/test/support/service/InternalServiceConfigTestServiceImpl.java
+++ src/test/java/tech/corefinance/common/test/support/service/InternalServiceConfigTestServiceImpl.java
@@ -2,7 +2,6 @@ package tech.corefinance.common.test.support.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import tech.corefinance.common.test.support.model.InternalServiceConfigTest;
 import tech.corefinance.common.test.support.repository.TestInternalServiceConfigRepository;
 
 @Service
