3941f57ee825350040d9615dd71a7e3076aa7de6
Doan Bao Trung <doanbaotrung@gmail.com>
2024-02-29 12:41:00 +0000

    Update docker compose scripts

--- share-resources/docker-scripts/docker-compose-env.bat
+++ share-resources/docker-scripts/docker-compose-env.bat
@@ -1,4 +1,6 @@
-set DB_PORT=5433
-set DB_HOST=host.docker.internal
+set DB_PORT=5432
+rem set DB_HOST=host.docker.internal
+set DB_HOST=postgres
 set COMPOSE_PARALLEL_LIMIT=2
 set COMPOSE_IGNORE_ORPHANS=true
+set DB_BIND_PORT=5433
