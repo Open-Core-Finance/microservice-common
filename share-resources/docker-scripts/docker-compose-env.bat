d8a38930e08ffba350cc3fc2ba5155efb4810c5e
Trung Doan <trung_bao.d@kbtgvn.tech>
2024-03-01 11:37:57 +0700

    Reduce docker replicated nodes

--- share-resources/docker-scripts/docker-compose-env.bat
+++ share-resources/docker-scripts/docker-compose-env.bat
@@ -1,6 +1,6 @@
 set DB_PORT=5432
 rem set DB_HOST=host.docker.internal
-set DB_HOST=postgres
+set DB_HOST=postgresql
 set COMPOSE_PARALLEL_LIMIT=2
 set COMPOSE_IGNORE_ORPHANS=true
 set DB_BIND_PORT=5433
